bdf53538d32aed88fa9cdfc70cebc0d9dca02add
thefipster <felix.reisch@live.de>
2025-05-15 13:36:55 +0200

    Added initial project with an api key authentication handler.

---
+++ src/api/FitHappens.WebApi/Auth/ApiKeyAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitHappens.WebApi.Auth
+{
+    public class ApiKeyAttribute : ServiceFilterAttribute
+    {
+        public ApiKeyAttribute()
+            : base(typeof(ApiKeyAuthorizationFilter)) { }
+    }
+}
---
+++ src/api/FitHappens.WebApi/Auth/ApiKeyAuthorizationFilter.cs
@@ -0,0 +1,28 @@
+﻿using FitHappens.WebApi.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FitHappens.WebApi.Auth
+{
+    public class ApiKeyAuthorizationFilter : IAuthorizationFilter
+    {
+        private const string ApiKeyHeaderName = "X-API-Key";
+
+        private readonly IApiKeyValidator _apiKeyValidator;
+
+        public ApiKeyAuthorizationFilter(IApiKeyValidator apiKeyValidator)
+        {
+            _apiKeyValidator = apiKeyValidator;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            string apiKey = context.HttpContext.Request.Headers[ApiKeyHeaderName];
+
+            if (!_apiKeyValidator.IsValid(apiKey))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
