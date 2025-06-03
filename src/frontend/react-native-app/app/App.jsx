48a0280a64f1eab0a82cd219988281836325fd7d
akapowl <52012296+akapowl@users.noreply.github.com>
2025-06-03 16:10:41 +0200

    002 - Fit Happens First
    Only Fit Happens App Screen
    
    Added
    
    - .dockerignore
    
    - assets: logos
    - assets: fonts: ubuntu
    
    Changed
    
    - app.json
    - index.js
    - App.tsx to App.jsx
    
    Created
    
    - app folder structure
    - basic colors styles object

---
+++ src/frontend/react-native-app/app/App.jsx
@@ -0,0 +1,92 @@
+
+import React from 'react'
+
+// ----- ----- ----- ----- ----- //
+
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+  useWindowDimensions
+} from 'react-native'
+
+// ----- ----- ----- ----- ----- //
+
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+// ----- ----- ----- ----- ----- //
+
+import colors from './styles/colors'
+
+// ----- ----- ----- ----- ----- //
+
+import logoDark from './assets/images/logo/logo_dark.png'
+import logoLight from './assets/images/logo/logo_light.png'
+
+// ----- ----- ----- ----- ----- //
+
+function App() {
+
+  const paddingHorizontal = '10%'
+
+  const isDarkMode = useColorScheme() === 'dark'
+
+  const schemeBG = { backgroundColor: isDarkMode ? colors.darker : colors.lighter }
+  const schemeText = { color: isDarkMode ? colors.lighter : colors.darker }
+
+  const window = Dimensions.get('window')
+  // const window = useWindowDimensions()
+
+  return (
+
+    <SafeAreaProvider>
+
+      <StatusBar barStyle={ isDarkMode ? 'light-content' : 'dark-content' } backgroundColor={schemeBG.backgroundColor} translucent={true} />
+
+      <View style={[schemeBG, styles.container, { width: '100%', height: window.height, paddingHorizontal: paddingHorizontal }]}>
+
+        <Image style={styles.logo} source={ isDarkMode ? logoLight : logoDark } />
+
+        <Text style={[schemeText, styles.text]}>Fit Happens.</Text>
+
+      </View>
+
+    </SafeAreaProvider>
+  )
+
+}
+
+// ----- ----- ----- ----- ----- //
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // flexDirection: 'column'
+  },
+
+  logo: {
+    height: '25%',
+    objectFit: 'contain'
+  },
+
+  text: {
+    fontSize: 24,
+    fontWeight: 600,
+    fontStyle: 'italic',
+    marginTop: -24,
+    marginLeft: 24
+  }
+
+})
+
+// ----- ----- ----- ----- ----- //
+
+export default App
--- src/frontend/react-native-app/index.js
+++ src/frontend/react-native-app/index.js
@@ -1,9 +1,16 @@
+
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent(appName, () => App);
+import { name} from './app.json'
+
+import App from './app/App'
+
+AppRegistry.registerComponent(name, function() {
+
+	return App
+
+})
