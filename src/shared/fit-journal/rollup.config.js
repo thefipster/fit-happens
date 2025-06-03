ed132975e9c57340696c9683bbfabb42f6a9236d
thefipster <felix.reisch@live.de>
2025-06-03 14:04:01 +0200

    updated fit-journal to generate single file output.

--- src/shared/fit-journal/rollup.config.js
+++ src/shared/fit-journal/rollup.config.js
@@ -1,20 +1,31 @@
-import typescript from 'rollup-plugin-typescript2';
-import resolve from '@rollup/plugin-node-resolve';
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/fit-journal.js',
-      format: 'esm',
-      sourcemap: true,
-    }
-  ],
-  plugins: [
-    resolve(),        
-    typescript({
-      tsconfig: './tsconfig.json',
-      clean: true
-    })
-  ]
-};
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/fit-journal.js",
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        clean: true
+      })
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/fit-journal.d.ts",
+      format: "es"
+    },
+    plugins: [dts()]
+  }
+];
